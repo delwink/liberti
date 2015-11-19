902dcb1e552a56927e7ef1307e2c2b9f89afbf16
David McMackins II <contact@mcmackins.org>
2015-11-18 21:41:17 -0600

    Print error string instead of code when background blit fails

--- src/skin.c
+++ src/skin.c
@@ -799,7 +799,7 @@ Skin_get_frame (Skin *self)
     {
       rc = SDL_BlitSurface (self->background, NULL, final, NULL);
       if (rc < 0)
-	error ("Failed to blit background: %d", rc);
+	error ("Failed to blit background: %s", SDL_GetError ());
     }
 
   return final;
