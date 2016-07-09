2c573379e632e6a336b7263f72130f4c65a856fe
David McMackins II <contact@mcmackins.org>
2016-07-09 16:44:09 -0500

    Fix copyright year

--- src/log.c
+++ src/log.c
@@ -1,6 +1,6 @@
 /*
  *  LiberTI - TI-like calculator designed for LibreCalc
- *  Copyright (C) 2015 Delwink, LLC
+ *  Copyright (C) 2015-2016 Delwink, LLC
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
