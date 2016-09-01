af89f3159841b1a1d972f32a88aca30600ec7e92
David McMackins II <contact@mcmackins.org>
2016-09-01 17:26:12 -0500

    Remove faulty code

--- src/tibtype.c
+++ src/tibtype.c
@@ -802,8 +802,6 @@ inverse (const TIB *t)
   if (NULL == temp)
     return NULL;
 
-  gsl_permutation p;
-  int signum;
   size_t i;
   switch (t->type)
     {
@@ -822,28 +820,7 @@ inverse (const TIB *t)
       return temp;
 
     case TIB_TYPE_MATRIX:
-      /* This needs work. The decomp and invert calls here will not work,
-         because `p` is not properly initialized for use with the matrix here.
-         The following call will have undefined behavior, tending to fail more
-         than succeed, because `p.size` is not equal to
-         `temp->value.matrix->size1`. */
-      tib_errno = gsl_linalg_complex_LU_decomp (temp->value.matrix, &p,
-                                                &signum);
-      if (tib_errno)
-        {
-          tib_decref (temp);
-          return NULL;
-        }
-
-      tib_errno = gsl_linalg_complex_LU_invert (temp->value.matrix, &p,
-                                                temp->value.matrix);
-      if (tib_errno)
-        {
-          tib_decref (temp);
-          return NULL;
-        }
-
-      return temp;
+      // SPILLS OVER!
 
     default:
       tib_decref (temp);
